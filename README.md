83983e81b5e3f5fd925ca75d457da19ea53f254d
Awrish <awrishkhan@gmail.com>
2023-03-12 16:35:15 -0400

    Initial setup of webpage, calendar max set to today and fetch request updated, api returns information for image request

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Astronomy Photos Project
 
 
-This project was developed to easily and quickly pull images from the NASA Astronomy Photo of the Day archive using NASA's Open API.
+This project was developed to easily and quickly pull images from the NASA Astronomy Photo of the Day archive using NASA's Open API, while allowing me to expiriment and get more comfortable with full-stack development.
+
---
+++ script.js
@@ -0,0 +1,39 @@
+const apiKey =  "Yrf09T9fD51i7pELjCJwgXG7gf4dON6QQl3euvuN";
+
+const todaysDate = new Date().toISOString().slice(0, 10);
+
+//const dateControl = document.querySelector('input[type="date"]');
+
+
+/** 
+let image = {
+    "apiKey": "Yrf09T9fD51i7pELjCJwgXG7gf4dON6QQl3euvuN",
+    fetchImage: function() {
+        fetch("https://api.nasa.gov/planetary/apod?api_key=" + this.apiKey + "&date=" + date)
+    }
+}
+
+*/
+
+function updateImage() {
+
+    var date;
+
+    const dateControl = document.querySelector('input[type="date"]');
+    date = dateControl.value;
+    console.log(dateControl.value);
+
+    var queryURLDate = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&date=" + date;
+
+    console.log(queryURLDate);
+
+    fetch(queryURLDate)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+}
+
+function test() {
+    var today = new Date().toISOString().split('T')[0];
+    document.querySelector('input[type="date"]').setAttribute('max', today);
+}
